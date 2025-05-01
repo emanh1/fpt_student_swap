63bcf6f6f4d0919149e0578c460c0042e4138228
emanh <cnttblog@gmail.com>
2025-05-01 10:12:40 +0000

    feat: add session management and configure passport for authentication

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,10 @@ import cors from "cors";
 import db from "./app/models/index.js"; 
 import swapRoutes from "./app/routes/swap.routes.js"; 
 import dotenv from "dotenv";
+import passport from "passport";
+import session from "express-session";
+import authRoutes from "./app/routes/auth.routes.js";
+import configurePassport from "./app/config/passport.config.js";
 
 dotenv.config();
 
@@ -10,16 +14,28 @@ const app = express();
 
 const corsOptions = {
     origin: process.env.CORS_ORIGIN,
+    credentials: true
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+configurePassport();
+
 app.get("/", (req, res) => {
     res.json({ message: "OK" });
 });
 
+authRoutes(app);
 swapRoutes(app);
 
 db.sequelize.sync().then(() => {
